681fbee547133156b5b567452a29e538b693a15b
MindVersal <antajchik@gmail.com>
2017-09-18 10:36:27 +0300

    10:33 18.09.2017

---
+++ rss/apps/rss_news/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RssNewsConfig(AppConfig):
+    name = 'rss.apps.rss_news'
--- rss/settings.py
+++ rss/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'rss.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'rss/apps/rss_news/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
