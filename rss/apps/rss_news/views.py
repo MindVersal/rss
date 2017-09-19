17917bec28ed72db01f6a04ae79aa78d133a68a6
MindVersal <antajchik@gmail.com>
2017-09-19 13:20:13 +0300

    13:20 19.09.2017

--- rss/apps/rss_news/views.py
+++ rss/apps/rss_news/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-import random
 
 # Create your views here.
 
 
 def index(request):
-    # return HttpResponse('Hi!')
-    number = random.randrange(0, 100)
+
     context = {
-        'value': 'Hello Python',
-        'number': str(number)
+
     }
-    return render(request, 'my_bootstrap3.html', context)
+    return render(request, 'index.html', context)
