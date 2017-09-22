effa8dc8eb9abfcd65d481fae619e9567adc3269
MindVersal <antajchik@gmail.com>
2017-09-22 12:02:00 +0300

    12:00 22.09.2017

--- rss/apps/rss_news/urls.py
+++ rss/apps/rss_news/urls.py
@@ -20,5 +20,5 @@ from rss.apps.rss_news import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^post/(?P<index>[0-9]+)/$', views.post),
-    url(r'^log_form$', views.log_form),
+    url(r'^log_form', views.log_form),
 ]
